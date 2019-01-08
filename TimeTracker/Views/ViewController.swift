433a9d595d531fcd7de7dcd667504c17e6c46f36
Lewis Valaitis <Lewis@Jamess-MacBook-Pro-4.local>
2019-01-08 20:44:05 +0000

    Initial commit.

---
+++ TimeTracker/Views/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TimeTracker
+//
+//  Created by Lewis Valaitis on 18/12/2018.
+//  Copyright © 2018 Lewis Valaitis. All rights reserved.
+//
+
+import UIKit
+
+internal final class ViewController: UIViewController { }
+internal extension ViewController {
+    @IBAction func startTimer() {
+        print("Tapped.")
+    }
+}
